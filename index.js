64012796d4b9dea235b6bf2879488f876224df0a
matheus <matheusmat2016@gmail.com>
2023-04-07 16:00:23 -0300

    commited

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const calcular = document.getElementById("calcular");
 
+function imc() {
+    const nome = document.getElementById("nome");
+    const altura = document.getElementById("altura");
+    const peso = document.getElementById("peso");
+    const resultado = document.getElementById("resultado");
 
+    if (nome !== '') {
+        
+    }
+}
+
+calcular.addEventListener('click', imc);
