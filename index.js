703c3959620dc95c590f9f3bc6334014711a1580
matheus <matheusmat2016@gmail.com>
2023-04-07 18:48:17 -0300

    modificado

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const calcular = document.getElementById("calcular");
 
 function imc() {
-    const nome = document.getElementById("nome");
-    const altura = document.getElementById("altura");
-    const peso = document.getElementById("peso");
+    const nome = document.getElementById("nome").value;
+    const altura = document.getElementById("altura").value;
+    const peso = document.getElementById("peso").value;
     const resultado = document.getElementById("resultado");
 
-    if (nome !== '') {
-        
+    if (nome !== '' && altura !== '' && peso !== '') {
+
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+        let classificacao = '';
+
+        if (valorIMC < 18.5){
+            classificacao = 'abaixo do peso.';
+        } else if(valorIMC > 25){
+            classificacao = 'esta com o peso ideal.';
+        } else if (valorIMC > 30){
+            classificacao='um pouco acima do peso.';
+        } else if (valorIMC > 35){
+            classificacao = 'obesidade grau I';
+        } else if (valorIMC > 40){
+            classificacao = 'obesidade grau II';
+        }else if(valorIMC > 42) {
+            classificacao = 'com obesidade grau III. Muito cuidado.';
+        }else{
+            classificacao = ' com o peso muito abaixo do saudavel, cuidado';
+        } 
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+    } else {
+        resultado.textContent = 'Preencha todos os campos';
     }
-}
+};
 
 calcular.addEventListener('click', imc);
